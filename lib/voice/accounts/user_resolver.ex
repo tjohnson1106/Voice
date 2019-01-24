1a75795dede197707efda8142e7ba86a5e17a5a9
Thomas Johnson <tjohnson1106@gmail.com>
2019-01-24 13:00:55 -0500

    created user resolver

---
+++ lib/voice/accounts/user_resolver.ex
@@ -0,0 +1,14 @@
+defmodule Voice.Accounts.UserResolver do
+  alias Voice.Accounts
+
+  def all(_args, _info) do
+    {:ok, Accounts.list_users()}
+  end
+
+  def find(%{email: email}, _info) do
+    case Accounts.get_user_by_email(email) do
+      nil -> {:error, "User email #{email} not found!"}
+      user -> {:ok, user}
+    end
+  end
+end
